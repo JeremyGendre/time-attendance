d651de199dedb496b97a198a6c51934b1fb27e25
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-04-19 10:25:02 +0200

    fix: refactor js app to main

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('main', './assets/main.js')
     .addEntry('header', './assets/styles/header.css')
     .addEntry('login', ['./assets/styles/login.css'])
 
