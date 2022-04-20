debc8f590dbce1c6ac7b39ef692d6202cba9d4da
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-04-20 18:37:03 +0200

    fix: useTime hook

--- assets/js/utils/hooks/useTime.js
+++ assets/js/utils/hooks/useTime.js
@@ -4,11 +4,10 @@ import {getDateString} from "../date";
 export default function useTime(){
     const [time, setTime] = useState(getDateString(new Date()));
 
-    const interval = setInterval(() => {
-        setTime(getDateString(new Date()));
-    },1000);
-
     useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(getDateString(new Date()));
+        },1000);
         return () => clearInterval(interval)
     },[]);
 
