dadb4bb0017dcff57ac74c91eeb907f5c368df26
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-04-20 20:07:21 +0200

    feat: fetch extra tickings

--- assets/js/app/ExtraTicking.js
+++ assets/js/app/ExtraTicking.js
@@ -1,15 +1,29 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 export default function ExtraTicking(){
+    const [fetching, setFetching] = useState(true);
+    const [extraTicking, setExtraTicking] = useState([]);
 
     useEffect(() => {
-        //axios.get(`/extra-ticking/today`).then().catch().finally()
+        axios.get(`/extra-ticking/today`)
+            .then(result => {
+                setExtraTicking(result.data.extraTickings);
+            })
+            .catch(console.error)
+            .finally(() => setFetching(false))
     },[]);
 
+    if(fetching) return <div className="d-flex"><div className="my-auto mr-1">Chargement...</div><div className="loader simple-loader"/></div>;
+
     return (
-        <div>
-            Extra tickings
+        <div className="extra-tickings-container">
+            <div>Pointages exceptionnels :</div>
+            <div>{extraTicking.map(extraTicking => (
+                <div key={`extra-ticking-${extraTicking.id}`}>
+                    <div>{extraTicking.startDate} - {extraTicking.endDate}</div>
+                </div>
+            ))}</div>
         </div>
     );
 }
--- src/Controller/ExtraTickingController.php
+++ src/Controller/ExtraTickingController.php
@@ -3,15 +3,37 @@
 
 namespace App\Controller;
 
+use App\Repository\TickingRepository;
+use App\Service\Utils\DateTime;
+use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 
 /**
  * @Route("/extra-ticking")
  * Class ExtraTickingController
  * @package App\Controller
  */
-class ExtraTickingController
+class ExtraTickingController extends BaseAbstractController
 {
-    public function getTodayExtraTicking()
-    {}
+    /**
+     * @Route("/today", name="get_extra_ticking_of_the_day", methods={"GET"})
+     * @param TickingRepository $tickingRepository
+     * @return JsonResponse
+     * @throws Exception
+     * @throws ExceptionInterface
+     */
+    public function getTodayExtraTicking(TickingRepository $tickingRepository):JsonResponse
+    {
+        $todayTicking = $tickingRepository->findOneBy(['user' => $this->getUser(), 'tickingDay' => new DateTime()]);
+        $extraTickings = $todayTicking ? $todayTicking->getExtraTickings() : [];
+        return new JsonResponse([
+            'extraTickings' => $this->getSerializer()->normalizeMany($extraTickings,null,[
+                'groups' => 'main',
+                DateTimeNormalizer::FORMAT_KEY => 'H:i'
+            ])
+        ]);
+    }
 }
